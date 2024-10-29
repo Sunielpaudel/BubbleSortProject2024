f76fe1f537988fb17093a6fbaab47ec2f1fb43bf
Suniel <Suniel.paudel2047@gmail.com>
2024-10-29 11:19:03 -0700

    added .gitignore and BubbleSortProject folder.

---
+++ BuubleSortProject/maxFinder.hpp
@@ -0,0 +1,10 @@
+#ifndef MAXFINDER_HPP
+#define MAXFINDER_HPP
+
+#include "bubbleSortPro.hpp"  // Include bubbleSortPro for sorting
+
+namespace maxFinder {
+    int findMax(int arr[], int n);
+}
+
+#endif // MAXFINDER_HPP
